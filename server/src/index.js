d6cc6b682664947d98204aafcc42682264dbc70a
halisonvitorino <halisonvitorino@yahoo.com.br>
2023-09-21 11:03:16 -0300

    Ver 01

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,37 @@
-//TODO
+const { ApolloServer }          = require ("@apollo/server");
+const { startStandaloneServer } = require ("@apollo/server/standalone");
+const { addMockToSchema }       = require ("@graphql-tools/mock");
+const { makeExecutableSchema }  = require ("@graphql-tools/schema");
+const typeDefs                = require ("./schema");
+
+const mocks = {
+    Track: () => ({
+      id: () => "track_01",
+      title: () => "Astro Kitty, Space Explorer",
+      author: () => {
+        return {
+          name: "Grumpy Cat",
+          photo:
+            "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+        };
+      },
+      thumbnail: () =>
+        "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+      length: () => 1210,
+      modulesCount: () => 6,
+    }),
+  };
+
+async function startApolloServer() {
+    const server = new ApolloServer ({
+        schema: addMockToSchema({
+            schema: makeExecutableSchema({ typeDefs }),
+            mocks,
+        }),
+    });
+    const {url}  = await startStandaloneServer(server);
+    console.log(`
+        Server online
+        Query at ${url}
+        `);
+}
